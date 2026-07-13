c6666c4830642dfa626d8fc7c90eb89e185d6bfc
Reedvejee H. Kiamco <ocmaikreedvejee6@gmail.com>
2026-07-13 15:59:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.getElementById("calculateBtn").onclick = function () {
 // Background Slideshow
 const images = [
   "images/background1.jpg",
-  "images/background2.jpg",
+  "images/background6.jpg",
   "images/background3.jpg",
   "images/background4.jpg",
   "images/background5.jpg",
